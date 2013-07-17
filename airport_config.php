99e1b7a6509cee544368403b4656fc194dfe6349
seinechen <jin.chen@flightaware.com>
2013-07-17 11:02:13 -0500

    change password and username

--- airport_config.php
+++ airport_config.php
@@ -2,8 +2,8 @@
 
 define("AIRPORT", "KAUS");
 define("AIRPORT_FULLNAME", "Austin-Bergstrom International Airport");
-define("USERNAME", "JinChen");
-define("APIKEY", "1ca7834f27488d5fd66e84a341415611535c5725");
+define("USERNAME", "USERNAME");
+define("APIKEY", "PASSWORD");
 define("DISPLAY_OPTION", '2');
 define("DIS_TIME", '0');
 define("REFRESH_PERIOD", 5000);
